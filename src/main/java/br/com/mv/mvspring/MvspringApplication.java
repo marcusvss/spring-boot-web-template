11fc8da0fa6fe0b7260dd1a08444859ea98abb81
Marcus Sales <marcus.vss88@gmail.com>
2019-01-29 17:25:36 -0200

    SpringbootWeb Project

---
+++ src/main/java/br/com/mv/mvspring/MvspringApplication.java
@@ -0,0 +1,14 @@
+package br.com.mv.mvspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvspringApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/mv/mvspring/MvspringApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.mv.mvspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
