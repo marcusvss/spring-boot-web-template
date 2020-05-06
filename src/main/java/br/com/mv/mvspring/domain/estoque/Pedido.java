1c94f3d2402f4c38ea394c8296c3927aa44e7b43
Marcus Sales <msales@confidencecambio.com.br>
2020-05-06 15:44:08 -0300

    Mapeamento de OneToOne Pedido e Pagamento

--- src/main/java/br/com/mv/mvspring/domain/estoque/Pedido.java
+++ src/main/java/br/com/mv/mvspring/domain/estoque/Pedido.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 public class Pedido extends Entidade implements Serializable {
@@ -31,4 +32,63 @@ public class Pedido extends Entidade implements Serializable {
     @JoinColumn(name = "endereco_id")
     private Endereco endereco;
 
+    public Pedido() {
+    }
+
+    public Pedido(Long id, Date dataPedido, Pagamento pagamento, Cliente cliente, Endereco endereco) {
+        super(id);
+        this.dataPedido = dataPedido;
+        this.pagamento = pagamento;
+        this.cliente = cliente;
+        this.endereco = endereco;
+    }
+
+    public Date getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(Date dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Pedido pedido = (Pedido) o;
+        return Objects.equals(dataPedido, pedido.dataPedido) &&
+                Objects.equals(pagamento, pedido.pagamento) &&
+                Objects.equals(cliente, pedido.cliente) &&
+                Objects.equals(endereco, pedido.endereco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), dataPedido, pagamento, cliente, endereco);
+    }
 }
--- src/main/java/br/com/mv/mvspring/domain/financeiro/Pagamento.java
+++ src/main/java/br/com/mv/mvspring/domain/financeiro/Pagamento.java
@@ -1,23 +1,30 @@
 package br.com.mv.mvspring.domain.financeiro;
 
-import br.com.mv.mvspring.domain.common.Entidade;
 import br.com.mv.mvspring.domain.estoque.Pedido;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import java.io.Serializable;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public class Pagamento extends Entidade implements Serializable {
+public class Pagamento implements Serializable {
 
     private static final long serialVersionUID = 3069754311194660835L;
+
+    @Id
+    private Long id;
 
     private Integer estadoPagamento;
 
     @OneToOne
+    @JoinColumn(name = "pedido_id")
+    @MapsId
     private Pedido pedido;
 
     public Pagamento(Integer estadoPagamento) {
@@ -30,5 +37,25 @@ public class Pagamento extends Entidade implements Serializable {
 
     public void setEstadoPagamento(EstadoPagamento estadoPagamento) {
         this.estadoPagamento = estadoPagamento.getValor();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEstadoPagamento(Integer estadoPagamento) {
+        this.estadoPagamento = estadoPagamento;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
     }
 }
